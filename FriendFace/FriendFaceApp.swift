3be4767d87f49a0b8d72f0c803f8d03fb48c29c1
Artem Bolotov <teeemon@gmail.com>
2023-02-26 08:43:42 +0300

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by artembolotov on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
